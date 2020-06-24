3334fc49e7356bd2a89185817c94f3b22b84c596
zaru <zarutofu@gmail.com>
2020-06-24 21:53:26 +0900

    Delete unused file

--- todo-php/day2/create.sql
+++
@@ -1,7 +0,0 @@
--- todos テーブルが存在していない場合、新規作成する
-CREATE TABLE IF NOT EXISTS todos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, -- プライマリーキーを ID に。値を指定しなくても自動で連番を発行してくれる AutoIncrement を設定
-  content TEXT NOT NULL,                -- ToDo の内容を保存する
-  done BOOLEAN NOT NULL DEFAULT false,  -- ToDo が完了したかどうかのフラグ。Boolean なので true か false が入る
-  created_at DATETIME NOT NULL          -- ToDo を作成した日時を保存する
-);
